05401c62176416b5699a3e90df967309300d9aec
Mohan Devarkonda <mdevarak@hotmail.com>
2016-12-17 15:48:48 -0500

    Comment added on the main method

--- MohansHello/src/Welcome.java
+++ MohansHello/src/Welcome.java
@@ -1,10 +1,14 @@
 
 public class Welcome {
 
+	/**
+	 * Main method to perform the job of printing message
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello from Mohan");
+		System.out.println("Hello from Mohan.");
 	}
 
 }
